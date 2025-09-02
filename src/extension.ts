c3cdd25b220f5a704b3d044b84a0ac95f30f3838
DrKim <acebunny00@gmail.com>
2025-09-02 22:11:05 +0900

    [기능] 탐색기 컨텍스트 메뉴 실행
    `package.json`: 컨텍스트 메뉴 추가
    `extension.ts`: 구현
    `README.md`: 설명

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,19 @@
+import { exec } from 'child_process';
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 	const openGeminiDisposable = vscode.commands.registerCommand("gemini.cli", () => {
 		vscode.window.createTerminal({ name: "Gemini CLI", shellPath: "powershell.exe", shellArgs: ["-NoExit", "-Command", "& gemini --model gemini-2.5-flash --yolo --all-files --checkpointing"] }).show();
+	});
+
+	const openGeminiInPowershellDisposable = vscode.commands.registerCommand("gemini.cli.inPowerShell", (uri: vscode.Uri) => {
+		const path = uri.fsPath;
+		exec(`start powershell -NoExit -Command "cd '${path}'; gemini --model gemini-2.5-flash --yolo --all-files --checkpointing"`);
+	});
+
+	const openGeminiInGitBashDisposable = vscode.commands.registerCommand("gemini.cli.inGitBash", (uri: vscode.Uri) => {
+		const path = uri.fsPath;
+		exec(`start "" "C:\\Program Files\\Git\\git-bash.exe" -c "cd '${path}'; gemini --model gemini-2.5-flash --yolo --all-files --checkpointing"`);
 	});
 
 	const geminiStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
@@ -11,7 +22,7 @@ export function activate(context: vscode.ExtensionContext) {
 	geminiStatusBarItem.tooltip = "Gemini CLI Launcher";
 	geminiStatusBarItem.show();
 
-	context.subscriptions.push(openGeminiDisposable, geminiStatusBarItem);
+	context.subscriptions.push(openGeminiDisposable, openGeminiInPowershellDisposable, openGeminiInGitBashDisposable, geminiStatusBarItem);
 }
 
 export function deactivate() {}
